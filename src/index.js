bee23b94ada5e60219900fd03c8ac7e4b9600bd6
johnisr <34279434+johnisr@users.noreply.github.com>
2021-01-22 18:33:16 -0500

    Export Title fcommon component (#23)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import CustomTable from "./Table/CustomTable";
 import Datatable from "./Table/Datatable";
+import Title from "./Title/Title";
 
-export { CustomTable, Datatable };
+export { CustomTable, Datatable, Title };
 
 export default {
   CustomTable,
   Datatable,
+  Title,
 };
