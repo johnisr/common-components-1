8d590db80f614b9d78117f1ff83480e5131a9a03
johnisr <john.ramos@validere.com>
2021-08-12 10:08:16 -0400

    Add navbar component

---
+++ src/NavBar.js/ProfilePopover.js
@@ -0,0 +1,52 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+import "./ProfilePopover.css";
+
+const MenuItem = (props) => {
+  return (
+    <div
+      className={`profilePopover__menuItem ${
+        props.disabled ? "profilePopover__menuItem--disabled" : ""
+      }`}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const PropfilePopover = (props) => {
+  return (
+    <div className="profilePopover">
+      <MenuItem disabled={!props.onSignOut} onClick={props.onSignOut}>
+        Sign Out
+      </MenuItem>
+
+      {props.version && (
+        <>
+          <div className="profilePopover__menuDivider"></div>
+
+          <MenuItem disabled={true}>Version: {props.version}</MenuItem>
+        </>
+      )}
+    </div>
+  );
+};
+
+MenuItem.propTypes = {
+  /** The className given to the PropfilePopover */
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+PropfilePopover.propTypes = {
+  /** The className given to the PropfilePopover */
+  version: PropTypes.string,
+  onSignOut: PropTypes.func,
+};
+
+export default PropfilePopover;
