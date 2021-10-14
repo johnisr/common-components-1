e19725bc1ef06b2eba32f5926415c8ed3e49fd2e
Goran Tesic <goran.tesic@noubis.com>
2021-10-14 12:24:01 +0200

    [V360-8668] feat(typescript): migrate page component into typescript

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import * as PropTypes from "prop-types";
 import ReactLoader from "../Loader/ReactLoader";
 import FontAwesome from "react-fontawesome";
 import Title from "../Title/Title";
 import "./Page.css";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+
+type Props = {
+  className: string;
+  /** The style given to the Page Container */
+  style: Object;
+  /** The content to be displayed below the title */
+  children: React.ReactNode | React.ReactNode[];
+  /** The given title of the content */
+  title: React.ReactNode[] | React.ReactNode | string;
+  /** If present, displays a button that executes a function on click */
+  onClick: () => void;
+  /** If present, shows uses the ReactLoader component to show a loading image while content is loading */
+  loaded: boolean;
+  /** An array of { title, onClick } objects, with title being what is displayed
+   *  and onClick the function called when the breadcrumb is clicked
+   */
+  breadcrumbs: Array<string>;
+  /** The className applied to the breadcrumbs container */
+  headerClassName: string;
+};
 
 /** The default margins are 15px */
 const Page = ({
@@ -16,7 +35,7 @@ const Page = ({
   loaded,
   breadcrumbs,
   headerClassName = "",
-}) => {
+}: Props) => {
   return (
     <div className={`page ${className}`} style={style}>
       {breadcrumbs && (
@@ -48,34 +67,6 @@ const Page = ({
       )}
     </div>
   );
-};
-
-Page.propTypes = {
-  /** The className given to the Page Container */
-  className: PropTypes.string,
-  /** The style given to the Page Container */
-  style: PropTypes.object,
-  /** The content to be displayed below the title */
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]),
-  /** The given title of the content */
-  title: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-    PropTypes.string,
-  ]),
-  /** If present, displays a button that executes a function on click */
-  onClick: PropTypes.func,
-  /** If present, shows uses the ReactLoader component to show a loading image while content is loading */
-  loaded: PropTypes.bool,
-  /** An array of { title, onClick } objects, with title being what is displayed
-   *  and onClick the function called when the breadcrumb is clicked
-   */
-  breadcrumbs: PropTypes.array,
-  /** The className applied to the breadcrumbs container */
-  headerClassName: PropTypes.string,
 };
 
 export default Page;
