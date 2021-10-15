82b2db1b9486bc204e087dab914dd8886ab1444a
Goran Tesic <goran.tesic@noubis.com>
2021-10-15 13:26:08 +0200

    Extract PanelTypes to types folder

--- src/Panel/Panel.tsx
+++ src/Panel/Panel.tsx
@@ -2,21 +2,7 @@ import React from "react";
 import ReactLoader from "../Loader/ReactLoader";
 import Title from "../Title/Title";
 import "./Panel.css";
-
-type PanelProps = {
-  /** The className given to the Panel */
-  className: string;
-  /** The style given to the Panel */
-  style: Object;
-  /** The content to be displayed inside the Panel */
-  children: React.ReactNode | React.ReactNode[];
-  /** The given title for the Panel */
-  title: React.ReactNode[] | React.ReactNode | string;
-  /** If present, shows uses the ReactLoader component to show a loading image while content is loading */
-  loaded: boolean;
-  /** If present, executes the given function when the panel is clicked */
-  onClick?: React.MouseEventHandler<HTMLDivElement>;
-};
+import PanelTypes from "../types/Panel";
 
 const Panel = ({
   className = "",
@@ -25,7 +11,7 @@ const Panel = ({
   loaded,
   onClick,
   children,
-}: PanelProps) => {
+}: PanelTypes) => {
   const isClickable = onClick && (loaded === undefined || loaded);
 
   return (
--- src/types/Panel/index.tsx
+++ src/types/Panel/index.tsx
@@ -1,1 +1,18 @@
-// Your types here.
+import React from "react";
+
+type PanelTypes = {
+  /** The className given to the Panel */
+  className: string;
+  /** The style given to the Panel */
+  style: Object;
+  /** The content to be displayed inside the Panel */
+  children: React.ReactNode | React.ReactNode[];
+  /** The given title for the Panel */
+  title: React.ReactNode[] | React.ReactNode | string;
+  /** If present, shows uses the ReactLoader component to show a loading image while content is loading */
+  loaded: boolean;
+  /** If present, executes the given function when the panel is clicked */
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+};
+
+export default PanelTypes;
