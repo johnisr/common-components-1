7a1ab195f390aecf5b15c93f1b92998b56795efa
Goran Tesic <goran.tesic@noubis.com>
2021-10-15 13:18:46 +0200

    Extract PageType to types folder

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -4,26 +4,7 @@ import FontAwesome from "react-fontawesome";
 import Title from "../Title/Title";
 import "./Page.css";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
-
-type Props = {
-  className: string;
-  /** The style given to the Page Container */
-  style: Object;
-  /** The content to be displayed below the title */
-  children: React.ReactNode | React.ReactNode[];
-  /** The given title of the content */
-  title: React.ReactNode[] | React.ReactNode | string;
-  /** If present, displays a button that executes a function on click */
-  onClick: React.MouseEventHandler<HTMLDivElement>;
-  /** If present, shows uses the ReactLoader component to show a loading image while content is loading */
-  loaded: boolean;
-  /** An array of { title, onClick } objects, with title being what is displayed
-   *  and onClick the function called when the breadcrumb is clicked
-   */
-  breadcrumbs: Array<string>;
-  /** The className applied to the breadcrumbs container */
-  headerClassName: string;
-};
+import PageTypes from "../types/Page";
 
 /** The default margins are 15px */
 const Page = ({
@@ -35,7 +16,7 @@ const Page = ({
   loaded,
   breadcrumbs,
   headerClassName = "",
-}: Props) => {
+}: PageTypes) => {
   return (
     <div className={`page ${className}`} style={style}>
       {breadcrumbs && (
--- src/types/Page/index.tsx
+++ src/types/Page/index.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+
 // Your types here.
+type PageTypes = {
+  className: string;
+  /** The style given to the Page Container */
+  style: Object;
+  /** The content to be displayed below the title */
+  children: React.ReactNode | React.ReactNode[];
+  /** The given title of the content */
+  title: React.ReactNode[] | React.ReactNode | string;
+  /** If present, displays a button that executes a function on click */
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+  /** If present, shows uses the ReactLoader component to show a loading image while content is loading */
+  loaded: boolean;
+  /** An array of { title, onClick } objects, with title being what is displayed
+   *  and onClick the function called when the breadcrumb is clicked
+   */
+  breadcrumbs: Array<string>;
+  /** The className applied to the breadcrumbs container */
+  headerClassName: string;
+};
+
+export default PageTypes;
