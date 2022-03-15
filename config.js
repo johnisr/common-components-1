4abac8d51aa0e37b5b21e03057b085e6cb1a0e13
johnisr <34279434+johnisr@users.noreply.github.com>
2022-03-15 10:49:33 -0400

    Remove back to hubs button and change homeTabText to validere logo (#490)
    * Remove back to hubs button and change homeTabText to validere logo
    
    * Fix tests that expect the removed back to hubs button

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const config = {
   VALIDERE_ICON_URL: "https://validere.com/wp-content/uploads/logo_icon.png",
+  VALIDERE_LOGO_URL:
+    "https://validere.com/wp-content/uploads/logo_white_text.png",
   DATE_FORMAT: "YYYY-MM-DD",
   TIME_FORMAT: "HH:mm:ss",
   DATETIME_FORMAT: "YYYY-MM-DD HH:mm:ss z",
--- src/types/Sidebar/index.tsx
+++ src/types/Sidebar/index.tsx
@@ -29,10 +29,6 @@ export type SidebarType = TabDetail & {
   onSignOut: React.MouseEventHandler<HTMLDivElement>;
   /**  Boolean function determining is sidebar is currently expanded */
   isExpanded: boolean;
-  /** Text describing the current web app */
-  homeTabText: string;
-  /** The function called when the back link is clicked */
-  onBackClick: React.MouseEventHandler<HTMLButtonElement>;
   /** The function called when View profile is clicked */
   onProfileClick: React.MouseEventHandler<HTMLDivElement>;
 };
